ccde22d51aa4e3967635701df2ded46a4d7baae9
Настя Ким <nastytrifono03@gmail.com>
2022-11-30 15:01:07 +0300

    Добавьте файлы проекта.

---
+++ Russia/NavigateFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Russia
+{
+    internal class NavigateFrame
+    {
+        public static Frame perehod;
+    }
+}
